33104213554bcd119f584c5bb153611fa43826e2
路小仁 <1622991435@qq.com>
2018-03-27 19:30:30 +0800

    完成了信息管理系统的侧边栏和家庭作业

--- demo/inforsystem/js/style.js
+++ demo/inforsystem/js/style.js
@@ -4,11 +4,11 @@
 
 
 
-function changelist($this,element) {/*改变列表的状态*/
+function changelist($this,element,flag) {/*改变列表的状态*/
 
     var height = element.style.height;
-
-    if(height === '0px' || height === ''){
+    console.log(height,flag,'1');
+    if((height === '0px' || height === '')&&flag === false){/*当初始高度为隐藏时高度时设置动画为展开动画*/
         element.style.animation='asideFrameShow   .2s linear  forwards';/*设置列表页展开动画*/
         element.style.height='144px';                                   /*重置列表页高度*/
         $this.style.backgroundColor='#fff';
@@ -20,7 +20,7 @@ function changelist($this,element) {/*改变列表的状态*/
 
 
     }
-    else {
+    else if((flag === true)&&(height!== '0px')){/*当初始高度为展开时的高度时设置动画*/
         element.style.animation='asideFrameHidden .2s linear  forwards';
         element.style.height='0px';
         $this.style.backgroundColor='#f9f9f9';
@@ -29,6 +29,24 @@ function changelist($this,element) {/*改变列表的状态*/
         $this.style['border-left-color']='#e5e5e5';
         $this.style.color='#616161';
 
+    }
+    else if((flag === true)&&(height === '0px')){/*初始高度为展开时时高度的第二次点击*/
+        element.style.animation='asideFrameShow .2s linear  forwards';
+        element.style.height='144px';
+        $this.style.backgroundColor='#fff';
+        $this.style['border-left-width']='3px';
+        $this.style['border-left-style']='solid';
+        $this.style['border-left-color']='#3382af';
+        $this.style.color='#616161';
+    }
+    else if((flag===false)&&(height!== '0px')){/*初始高度为隐藏时的第二第点击*/
+        element.style.animation='asideFrameHidden .2s linear  forwards';
+        element.style.height='0px';
+        $this.style.backgroundColor='#f9f9f9';
+        $this.style['border-left-width']='1px';
+        $this.style['border-left-style']='solid';
+        $this.style['border-left-color']='#e5e5e5';
+        $this.style.color='#616161';
     }
 
 }
